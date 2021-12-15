b5d1ff9a7b0d6a2f8f633f644f2eb8e382451e39
Tommy <happytommyl@gmail.com>
2021-12-15 18:07:59 +0800

    generalize

--- day1/day1.py
+++ day1/day1.py
@@ -5,19 +5,19 @@ with open('input.txt') as f:
     readings = f.readlines()
 readings = [int(line.rstrip('\n')) for line in readings]
 
+
 def num_increases(num: Iterator[int]):
     return sum((b > a for a, b in itertools.pairwise(num)))
 
-def take_three(it: Iterator[int]):
-    return ((it[i-2:i+1]) for i in range(2, len(it)))
 
-def sum_three(nums: Iterator[int]):
-    return (sum(i) for i in take_three(nums))
+def take_n(num, n):
+    return ((num[i-(n-1):i+1]) for i in range(n-1, len(num)))
 
 
 def main():
     print('part1:', num_increases(readings))
-    print('part2:', num_increases(sum_three(readings)))
+    print('part2:', num_increases(map(sum, take_n(readings, 3))))
+
 
 if __name__ == '__main__':
     main()
